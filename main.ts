26892b723fee8537cac17f8f1d96d4dcb46f0904
xXCarlosXx08 <130647173+xXCarlosXx08@users.noreply.github.com>
2023-05-08 08:50:30 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,10 +2,18 @@ input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
     radio.sendString("HOLA")
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
+    radio.sendString("Messirve")
+})
 radio.onReceivedString(function (receivedString) {
-    basic.showIcon(IconNames.Duck)
+    basic.showIcon(IconNames.Tortoise)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    radio.sendValue("name", 0)
+})
+radio.onReceivedValue(function (name, value) {
+    basic.showIcon(IconNames.Heart)
 })
 radio.setGroup(17)
-basic.forever(function () {
-	
-})
